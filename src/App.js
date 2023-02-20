343bdfcda6a2ab393520cc54e9acbc8d17180e23
Sepehr Katebi <sepehrkatebi61@gmail.com>
2023-02-20 17:21:08 +0400

    Final commit 1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    color : ${({ theme: { color } }) => color};
+    background-color : ${({ theme: { backgroundColor } }) => backgroundColor};
+    padding: 1rem;
+    font-size: 1.375rem;
+    text-align: center;
+  }
+`;
+
+const ModifiableShape = styled.div.attrs(({ $borderRadius }) => ({
+  style: {
+    borderRadius: $borderRadius + "%",
+  },
+}))`
+  background-color: green;
+  text-align: center;
+  width: 300px;
+  line-height: 300px;
+  margin: 1rem;
+  overflow: hidden;
+`;
+ModifiableShape.defaultProps = {
+  $borderRadius: 0,
+};
+
+const Button = styled.button`
+  cursor: pointer;
+`;
+
+const theme = {
+  dark: {
+    backgroundColor: "black",
+    color: "white",
+  },
+  light: {
+    backgroundColor: "white",
+    color: "black",
+  },
+};
 
 function App() {
+  const [changeTheme, setChangeTheme] = useState(false);
+  const [BRvalue, setBRvalue] = useState(0);
+  let intervalRef = useRef(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme[changeTheme ? "light" : "dark"]}>
+      <GlobalStyles />
+      <label>
+        Change Theme
+        <input
+          type="checkbox"
+          onChange={(e) => setChangeTheme((val) => !val)}
+        />
+      </label>
+      <ModifiableShape $borderRadius={BRvalue}>
+        Border Radius: {BRvalue}%
+      </ModifiableShape>
+      <Button
+        onMouseDownCapture={(e) => {
+          console.log(e);
+          if (e.button !== 0) return;
+          if (intervalRef.current != null) clearInterval(intervalRef.current);
+          intervalRef.current = setInterval(() => {
+            setBRvalue((val) =>
+              val === 50 ? val : parseFloat((val + 0.1).toFixed(2))
+            );
+          });
+        }}
+        onMouseUpCapture={(e) => {
+          clearInterval(intervalRef.current);
+        }}
+        onMouseOutCapture={(e) => {
+          clearInterval(intervalRef.current);
+        }}
+      >
+        Increase
+      </Button>
+      <Button
+        onMouseDownCapture={(e) => {
+          console.log(e);
+          if (e.button !== 0) return;
+          if (intervalRef.current != null) clearInterval(intervalRef.current);
+          intervalRef.current = setInterval(() => {
+            setBRvalue((val) =>
+              val === 0 ? val : parseFloat((val - 0.1).toFixed(2))
+            );
+          });
+        }}
+        onMouseUpCapture={(e) => {
+          clearInterval(intervalRef.current);
+        }}
+        onMouseOutCapture={(e) => {
+          clearInterval(intervalRef.current);
+        }}
+      >
+        Decrease
+      </Button>
+    </ThemeProvider>
   );
 }
 
